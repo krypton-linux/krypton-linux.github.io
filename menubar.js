4bcaeea159f3a3b5ebc6bc6584f552acd6ab6803
Itsuki0222 <175463719+Itsuki0222@users.noreply.github.com>
2024-09-16 20:46:06 +0900

    Create menubar.js

---
+++ menubar.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let currentItem = document.querySelector('.current-item');
+    let gnavListItems = document.querySelectorAll(".gnav__list__item");
+    let menuBar = document.querySelector('.menu__bar');
+
+    gnavListItems.forEach(function(item) {
+    item.addEventListener("mouseover", function() {
+      menuBar.style.width = item.offsetWidth + "px";
+      menuBar.style.left = item.offsetLeft + "px";
+    });
+
+    item.addEventListener("mouseout", function() {
+      if (currentItem) {
+        menuBar.style.width = currentItem.offsetWidth + "px";
+        menuBar.style.left = currentItem.offsetLeft + "px";
+      } else {
+        menuBar.style.width = "0";
+      }
+    });
+  });
+});
